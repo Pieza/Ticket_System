ab4a4f10b2955aa1d36b1430f96de1276f085d75
Pieza en la U <ulacit@docente.ulacit.ac.cr>
2019-03-27 20:41:35 -0600

    Metodo de extracción de ticketes

--- src/proyectofinal/Estructuras/Cola/Cola.java
+++ src/proyectofinal/Estructuras/Cola/Cola.java
@@ -1,6 +1,7 @@
 package proyectofinal.Estructuras.Cola;
 
 import javax.swing.JOptionPane;
+import proyectofinal.Objetos.Ticket;
 
 public class Cola {
 
@@ -71,26 +72,24 @@ public class Cola {
         return false;
     }
 
-    public void extrae(String modelo) {
-        //creamos las variables aix y auxAnterior que van a ser el frente de la cola
+    /**
+     * Se extrae el tickete por id de usuario
+     * @param idUsuario
+     * @return 
+     */
+    public String[] extrae(int idUsuario) {
+        //creamos las variables aix que va a ser el frente de la cola
         Nodo aux = frente;
-        Nodo auxAnterior = frente;
-        //la variable eliminado nos va a controlar las iteraciones
-        boolean eliminado = false;
-        while (eliminado == false) {
-            String modeloEncontrado = aux.getDato().getAsignadolvl1().getNombre().toUpperCase().trim();
-            if (modelo.toUpperCase().trim().contains(modeloEncontrado)) {
-                //cuando encontramos la coincidencia del modelo le asignamos a auxAnterior que el dato de atras va a ser el dato que le sigue a aux
-                auxAnterior.setAtras(aux.getAtras());
-                //borramos aux
-                aux.setAtras(null);
-                eliminado = true;
-            } else {
-                //cuando no encuentra el dato guardamos aux en auxanterior para asi iterar y no olvidar la posicion de donde venimos
-                auxAnterior = aux;
+        String resultado = "";
+        while (aux != null) {
+            if(aux.getDato().getAsignadolvl1().getId() == idUsuario){
+                resultado += aux.getDato() + ";";
             }
             //avanzamos al dato siguiente
             aux = aux.getAtras();
         }
+        
+        // retorna null si no encuentra tickets
+        return resultado.split(";");
     }
 }
